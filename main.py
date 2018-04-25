c962b2498fba447add78ecdad6d540ecd4b27ce9
Tyler-Joshua Dryden <dryden.inc718@gmail.com>
2018-04-25 00:17:54 -0400

    Divided Code For Readability & Efficiency
    Separated code into functions and deleted a lot of comments/unnecessary code. Now I will work to make Lan_Tran translate tenses properly. In addition I will add many new words to the lexicon.

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.clock import Clock
 from kivy.lang import Builder
- 
+
 
 lexicon = {
         #adjectives
 
         'forever':'plirtjhir', 'all':'plírt', 'alot':'plirt', 'probably':'pírbli', 'red':'riscirt', 'stupid':'ritter',
         'dark':'nwumei', 'night':'nwumweijhir','evening':'nwumweijhirplir', 'stagnantly':'nwujhirli',
-        'next':'jonír', "easy": "fajile", 'easy going': 'fajweh', 'skinny':'flirc', 'fat':'gírd', 'fatness':'girjírn', 
+        'next':'jonír', "easy": "fajile", 'easy going': 'fajweh', 'skinny':'flirc', 'fat':'gírd', 'fatness':'girjírn',
         'overweight':'girplir', 'obease':'girplírt','fragile':"fajirl","easy": "nwuarmír", "a":"une",
         "white":",blankirt","thoroughly" : "bonírli", 'bright':'ilum', 'charismatic':'jírsmiweh',
         'brown':'kacirt', 'hot':'kuh','hell-like':'kuhplirtli','black':'nekírt', 'powerless':'nwuhpír',
@@ -37,11 +37,11 @@ lexicon = {
          'north':'nwírh','discord': 'nwuhjarmijírn', 'bad':'nwuhbonír', 'life-essence':'nduhweh', 'vagina':'nduzi',
         'girl':'ngyal', 'everyday':'mjawehplirt', 'everyone':'mjawehplirt', 'nothing':'nuhweh', 'alone': 'nwuhnír',
         'medicine':'mjadírne', 'god':'mjah', 'logic':'mjahijwo', 'mother':'maira', 'beginning':'malijhír','start':'malír',
-        'monument': 'monírme', 'generation':'mjwajhírn', 'person':'mjaweh',  'homicide':'mjahwehjide', 
+        'monument': 'monírme', 'generation':'mjwajhírn', 'person':'mjaweh',  'homicide':'mjahwehjide',
         'man':'mwon', 'woman': 'mwomei', 'morning':'mwírn', 'stars' : ' mwesehly','day': 'mweíjhir','sun':'mwei',
         'garden': 'jadírne', 'moon':'lune', 'lamír': 'name', 'perfect': 'jírseplirt','hell on earth':'jaíplírt', 'charisma':'jírsmíjírn',
-        'drug':'jírg','master':'jukír', 'most high':'jukweh', 'sound':'jírtu', 'chaos':'jaís', 'partner': 'jenyír', 
-        'chapter':'isi', 'gold':'ír', 'money':'irjírn', 'east':'írste', 'communication' : 'ikjírn', 
+        'drug':'jírg','master':'jukír', 'most high':'jukweh', 'sound':'jírtu', 'chaos':'jaís', 'partner': 'jenyír',
+        'chapter':'isi', 'gold':'ír', 'money':'irjírn', 'east':'írste', 'communication' : 'ikjírn',
         'right now':'curojhir', "now" : "cur", "white": "blankirt", "today" : "curmweijhir", "almost-dead": "curnwuh",
          "cambír" : "change", "country": "citeir", 'city': 'citeírto', 'face': 'círa', 'heavens':'kushírme',
          "thoroughly":"bonírli", "greatness":"boníjírn", "brick":"brírck","young-boy":"bwoyírto" ,
@@ -72,7 +72,7 @@ lexicon = {
          "with many":"anírplirt", "all together" :"Anírplirt",  "is":"esír","at this time":"ahkírjir",
          "at times": "A-jhíer", "doing" : "a" , 'or' :'ih', 'really or yes': 'ihh', 'but':'píhr', "all-directions":'suh-ah-suh',
          "when":"ahkírjir", 'what': 'kírse', 'how':'kedgírse','who': 'mjahwírse',
-         'why':'pírcirse', 'because':'fircirse', 
+         'why':'pírcirse', 'because':'fircirse',
 
 #pros
 "he": "im","i":"mi",
@@ -82,7 +82,7 @@ lexicon = {
 "you all":"unnos",
 "they" : "dims",
 "my" :"a-mírn" ,
-"yours" :"a-vírm", 
+"yours" :"a-vírm",
 "his": "a-im ",
 "hers ":"a-smweí",
 "ourselves" :'nus-mírnes',
@@ -96,166 +96,129 @@ lexicon = {
 
         #negations:
 
-        "no": "buh", "not": "bun", 
+        "no": "buh", "not": "bun",
 
 }
 
 pastMark = ['ed',]
-past = 0
 
 #individual characters List
+
 indCharsList = []
+wList3 = ''
+regChars = []
+past = 2 > 100
+pastProps = []
+
+wList2 = ''
+pastNow = ''
+
 
 class Query(BoxLayout):
 
-    
-    def engToLan(self):
-        global past
-        global indCharsList
-        #check for spaces in user input
-        #when space found split the string into list
-        #join each word into into list for each iteration
+        #splitter function will check for spaces in user input
+        #& when a space is found it will split the string into a list.
+        #Next it will join each element of the list for each iteration of response.
 
-        
+    def splitter(self):
+        global wList
+        global wAmt
+        global newList
+        global answer
+        global ANSWER
+
+        wList = self.ids.entry.text.split(' ')
+        wAmt = len(wList)
+        newList = []
+        answer = ' '.join(wList)
+        ANSWER = answer.lower()
+
+        for response in wList:
+
+            respond = response.lower()
+
+            if past == True:
+                print('kk')
+
+            if respond in lexicon:
+
+                newList.append(lexicon[respond])
+                fTrans = ' '.join(newList)
+                output = fTrans[:]
+                self.ids.result.text = output
+
+    def pastTense(self):
+
+            for chars in self.ids.entry.text:
+                indCharsList.append(chars)
+                print(indCharsList)
+
+                if chars == 'd':
+
+                    regChars.append(indCharsList)
+                    past = 1 < 2
+                    print('found a d')
+                    print(regChars)
+
+                    if past:
+                        print('past now')
+
+                        wList2 = str(regChars).split("ed")
+                        wList3 = str(wList2).join('ed')
+                        wList3 = []
+                        print('g')
+                        print(wList3)
+
+
+    def engToLan(self):
+        global indCharsList
+        global wList2
+        global wList3
+        global past
+        global pastNow
+        global pastProps
+
+
 
         if ' ' in self.ids.entry.text:
-            wList = self.ids.entry.text.split(' ')
-            wAmt = len(wList)
-            newList = [] 
-            answer = ' '.join(wList)
-            ANSWER = answer.lower()
+            self.splitter()
 
+        if 'ed ' in self.ids.entry.text:
+                self.pastTense()
         # checking if this is past tense
 
         # split entry into indiv chars if the last two chars in the potential past tense
         #words are ed. this is one way that i am handling the past tense. NEed to figure
         #something out for irregular past tense verbs
-            
-            if 'ed' in self.ids.entry.text:
-                for chars in self.ids.entry.text:
-                
-                    if chars =='e':
-                        
-                        indCharsList.append('e')
-                        print('found an e')
-                        
 
-                        #if indCharsLi
 
-                    if chars == 'd':
-                        indCharsList.append('d')
-                        past = 1 > 2
-                        print('found a d')   
-                            #print(indCharsList)
-
-                    else:
-                        #indCharsList.append(' ')
-                        print('nothing')
-
-               # print(indCharsList[-1])
-                #print(indCharsList[:-2])
-
-                if indCharsList[-1:] == 'd' and indCharsList[:-2] == 'e':
-                  
-                        
-                    print(past)
-                    past = 1 < 2
-                        
-                    while past:
-                        wList2 = str(indCharsList[:]).split('d')
-                        wList3 = str(wList2[:]).split(' ')
-                        pastProps = []
-                        pastProps.append(wList2)
-                        pastNow = ','.join(wList2)
-                        #print(wList3)
-                        #print(indCharsList)
-                        #print(wList3)
-                        past = 1 > 2
-
-                        
-                else:
-                    past = 1 > 2
-                #    innerCharsList.remove('e')
-                    
-                    print('no e')
-
-                    print('future or present')
-
-    
-                
-                    '''
-                    for char in wList:
-                        n = char.split(' ')
-                        if 'ed' in n:
-                            print(n)
-                            print('ed found')
-                        
-                            pastProps = []
-                            #pastNow = ''.join(pastID)
-                            pastProps.append(wList)
-                            pastNow = pastProps.split(' ')
-                            
-                        #print(pastNow)
-                    print(pastNow)
-'''
-            
-                    
-                
-               # print(wList)
-            #print(wAmt)
-           
-            for response in wList:
-
-                respond = response.lower()
-                
-                if respond in lexicon:
-                    
-
-                    newList.append(lexicon[respond])
-                    print(newList)
-                    fTrans = ' '.join(newList)
-                    output = fTrans[:] 
-                    self.ids.result.text = output
-
-               # if 'ing' in self.ids.entry.text:
-                #    present = wList.append('a')
-
-                 
-        else:
-    #directions for if there is no space in the user input
+        def oneWord(self):
+            global oneWordResponse
 
             oneWordResponse = self.ids.entry.text.lower()
-            
+
             if oneWordResponse in lexicon:
-            
+
                 output = lexicon[oneWordResponse]
                 self.ids.result.text = output
                 wList = []
             else:
         #this is where the english to lantousir conditions will be declared
-                
+
                 if 'í' in self.ids.entry.text:
                     output = self.ids.entry.text
                     #self.ids.result.text = output
-                    
 
                 else:
 
                     output = "Sorry that word doesn't exist. Try Again. Use - for compund words like 'little-brother.'"
                     self.ids.result.text = output
-                        
 
-    
-            
-                            
-                          
-           
-        
+
+
 class Lan_TranApp(App):
     def build(self):
         return Query()
 
 if __name__== "__main__":
     Lan_TranApp().run()
-            
-
